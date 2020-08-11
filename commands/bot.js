e4c07a77ac263ebc9fc963b64731c8b3db76d37d
Diogo Nunes <diogonspi@gmail.com>
2020-08-11 12:14:11 +0000

    Algumas coisinhas

--- commands/bot.js
+++ commands/bot.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
   const embed = new Discord.MessageEmbed()
     .setTitle(`Meu Repositório:
-    https://github.com/DiogoNSPI06/HYPEDBOT`)
+    https://github.com/DiogoNSPI06/HYPEDBOT1`)
     .setColor("RANDOM")
     .setFooter(`© Código de ! Diogo06™#2369`)
   message.channel.send(embed);
